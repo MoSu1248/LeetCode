fa8c9b86062812a2a7f23d307ed50b870c06ec41
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-05-15 11:14:51 +0200

    Time: 39 ms (71.39%), Space: 54.3 MB (10.55%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn){
+    const res = []; 
+
+    for(const i in arr){
+        res.push(fn(arr[i], Number(i)))
+    }
+
+    console.log(res);
+    return res; 
+
+
+}
+
