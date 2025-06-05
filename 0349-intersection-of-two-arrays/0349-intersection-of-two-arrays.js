8e44566af53d4bfc14763685bc50cd9be161c269
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-06-05 11:51:22 +0200

    Time: 2 ms (43.05%), Space: 54.4 MB (66.35%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    let x = 0; 
+    let y = 0; 
+
+    const newArr = []; 
+
+    while (x < nums1.length && y < nums2.length ) {
+        
+        if (nums1[x] === nums2[y]) {
+
+            if (!newArr.includes(nums1[x])) {
+                newArr.push(nums1[x])
+            }
+            x++ 
+            y++
+        }
+        else if(nums1[x] < nums2[y]){
+            x++
+        }
+        else{
+            y++ 
+        }
+
+    }
+
+   return newArr
+};
