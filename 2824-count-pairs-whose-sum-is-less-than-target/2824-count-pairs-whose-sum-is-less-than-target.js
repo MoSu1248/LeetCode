3e2d6b3e5b6d28b64a11df5b86e85810cee35900
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-07-30 20:43:40 +0200

    Time: 153 ms (5.09%), Space: 63.9 MB (5.09%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    
+let ans = 0; 
+
+for(let i = 0 ; i < nums.length - 1 ; i++){
+    for(let j = nums.length - 1; j > i; j--){
+        if(nums[i] + nums[j] < target){
+            console.log(nums[i] + nums[j])
+            ans++
+        }
+    }
+}
+
+return ans
+
+
+};
