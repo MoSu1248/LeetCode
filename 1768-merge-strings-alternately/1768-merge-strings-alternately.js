2fb9a11a30709f0c6d3e33a3f152a4f018d5ba74
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-08-04 23:03:34 +0200

    Time: 46 ms (38.14%), Space: 53.9 MB (48.25%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,8 +4,8 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-  let i = 0; // Pointer for arr1
-  let j = 0; // Pointer for arr2
+  let i = 0; 
+  let j = 0; 
   const mergedArray = [];
 
   while (i < word1.length && j < word2.length) {
@@ -18,13 +18,11 @@ var mergeAlternately = function(word1, word2) {
     }
   }
 
-  // Add any remaining elements from arr1
   while (i < word1.length) {
     mergedArray.push(word1[i]);
     i++;
   }
 
-  // Add any remaining elements from arr2
   while (j < word2.length) {
     mergedArray.push(word2[j]);
     j++;
