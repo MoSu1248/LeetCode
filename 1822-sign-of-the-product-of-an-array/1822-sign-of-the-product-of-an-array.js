90e5703991cae82fa2c29a42833fced351457004
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-08-26 18:41:45 +0200

    Time: 1 ms (57.91%), Space: 52.9 MB (97.98%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+
+    let ans = 1 ; 
+
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === 0){
+            return 0
+        }
+
+
+        ans*=nums[i]
+    }
+    
+    if(ans === 0 ) return 0;
+    if (ans > 0)return 1; 
+    else{
+        return -1
+    }
+};
