31d31f47f5d216bf583d99325f528d97a3f639e6
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-08-27 20:50:42 +0200

    Time: 6 ms (67.79%), Space: 58.3 MB (16.22%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let Horigin = 0;
+    let Vorigin= 0; 
+    let steps = moves.split('')
+
+
+   
+    
+    steps.forEach(number  =>{
+       
+switch (number) {
+    case "U":
+       Vorigin++ 
+        break;
+    case "D":
+        Vorigin--
+        break;
+    case "L":
+        Horigin++
+        break;
+    case "R":
+        Horigin--
+        break;
+    default:
+        break;
+}
+    });
+
+if(Horigin === 0 && Vorigin === 0){
+    return true
+}else {
+    return false
+}
+    
+};
