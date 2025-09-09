9dc9b811e719cae009856a8780b632e140b10829
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-09-09 23:08:03 +0200

    Time: 18 ms (8.64%), Space: 63.8 MB (5.69%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+   
+    let ans =  s.split(" ");
+    const finalAns =  [] ;
+
+    for(let i = 0  ; i < ans.length ; i++){
+        for(let j = ans[i].length -1 ; 0 <= j ; j--){
+            
+       finalAns.push(ans[i][j])
+     }
+     if(i === ans.length-1){
+        break
+     }
+     else{
+            finalAns.push(' '); 
+     }
+    }
+
+return finalAns.join('')
+};
