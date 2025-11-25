f1dbe9f293802bccea836354cec80f3338ce09cd
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-11-25 21:54:02 +0200

    Time: 2 ms (29.47%), Space: 58.3 MB (38.07%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+
+    const ans = [];  
+    for(let i = 0 ; i < nums.length ; i++  ){
+        ans.push(nums[i])
+    }
+
+    for(let i = 0 ; i < nums.length ; i++  ){
+        ans.push(nums[i])
+    }
+
+    return ans
+};
